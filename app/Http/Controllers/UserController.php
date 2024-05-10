c585d1f9faf433fec2ea74a67fcd8f894874d4d9
LoydNgei <loydngei@gmail.com>
2024-05-10 22:30:06 +0300

    User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login () {
+        return view('login');
+    }
+
+    public function register () {
+        return view('register');
+    }
+
+    public function loginpost () {
+
+    }
+
+    public function registerpost () {
+
+    }
+
+    public function logout () {
+
+    }
+}
