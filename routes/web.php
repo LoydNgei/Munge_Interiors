2eb43bf4f2e05894a13fdfc9094ce76e2f40be47
LoydNgei <loydngei@gmail.com>
2024-05-10 22:41:52 +0300

    App.css

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'home']);
 
+Route::get('/login', [UserController::class, 'login']);
 
+Route::get('/register', [UserController::class, 'register']);
