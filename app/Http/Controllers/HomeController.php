d855c8e40dd7a713a23c63a1f40351548b188415
LoydNgei <loydngei@gmail.com>
2024-05-11 23:30:30 +0300

    Chnaged home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home () {
-        return view('Home');
+        return view ('Home.Home');
     }
 
     public function search () {
