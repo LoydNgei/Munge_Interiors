a2f304e5be625381dcb0bcc8d5d207cc35b26494
LoydNgei <loydngei@gmail.com>
2024-05-11 23:32:07 +0300

    Layout.blade.php title change

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ecommerce Tailwind</title>
+        <title>Munge Interiors</title>
         <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
         <link href="{{ asset('css/min.css') }}" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,8 +19,7 @@
 
     <body>
         <main>
-            {{ slot }}
+            {{ $slot }}
         </main>
     </body>
-
 </html>
