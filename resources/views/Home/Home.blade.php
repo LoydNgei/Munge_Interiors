ea67e564cd4e428e6e07b0e35bf6ef6037b14f31
LoydNgei <loydngei@gmail.com>
2024-07-16 22:29:11 +0300

    home.blade.php

--- resources/views/Home/Home.blade.php
+++ resources/views/Home/Home.blade.php
@@ -1,4 +1,4 @@
 <x-homecomp.layout>
     <x-homecomp.navbar></x-homecomp.navbar>
-        <x-homecomp.banner></x-homecomp.banner>
+        {{-- <x-homecomp.banner></x-homecomp.banner> --}}
 </x-homecomp.layout>
