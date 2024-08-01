6430e3bc2324d153c8281ff4f864864fc36d07a8
LoydNgei <loydngei@gmail.com>
2024-08-01 23:37:49 +0300

    shipping table

--- database/migrations/2024_07_31_193710_create_shippings_table.php
+++ database/migrations/2024_07_31_193710_create_shippings_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('shippings', function (Blueprint $table) {
-            $table->id();
+            $table->id('shipping_id');
+            $table->foreignId('order_id')->constrained('orders', 'order_id');
+            $table->foreignId('user_id')->constrained('users', 'user_id');
+            $table->string('shipping_address');
+            $table->string('shipping_status');
+            $table->string('tracking_number');
+            $table->timestamp('shipped_at')->nullable();
+            $table->timestamp('delivered_at')->nullable();
             $table->timestamps();
         });
     }
