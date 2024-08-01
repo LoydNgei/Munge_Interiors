eacf4e7eaea050d7eb564136adb1cdf291be66e8
LoydNgei <loydngei@gmail.com>
2024-08-01 23:37:41 +0300

    reviews table

--- database/migrations/2024_07_31_193710_create_reviews_table.php
+++ database/migrations/2024_07_31_193710_create_reviews_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
+            $table->id('review_id');
+            $table->foreignId('product_id')->constrained('products', 'product_id');
+            $table->foreignId('user_id')->constrained('users', 'user_id');
+            $table->integer('rating');
+            $table->text('comment');
             $table->timestamps();
         });
     }
