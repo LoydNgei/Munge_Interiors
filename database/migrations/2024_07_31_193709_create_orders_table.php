a9ff914950f5fb180a680083933912cbf212480e
LoydNgei <loydngei@gmail.com>
2024-08-01 23:36:48 +0300

    orders table

--- database/migrations/2024_07_31_193709_create_orders_table.php
+++ database/migrations/2024_07_31_193709_create_orders_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->id('order_id');
+            $table->foreignId('user_id')->constrained('users', 'user_id');
+            $table->decimal('total_amount', 8, 2);
+            $table->string('order_status');
+            $table->string('payment_status');
             $table->timestamps();
         });
     }
