628fe1a554813948b37e66e35dbaaaed255d52bf
LoydNgei <loydngei@gmail.com>
2024-08-02 22:21:50 +0300

    edited product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,11 +1,22 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 class Product extends Model
 {
-    use HasFactory;
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'product_id');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class, 'product_id');
+    }
+
+    public function inventory()
+    {
+        return $this->hasOne(Inventory::class, 'product_id');
+    }
 }
