9b372363255a4ca8e814fd1c1860a47dba7c5766
LoydNgei <loydngei@gmail.com>
2024-08-05 23:07:11 +0300

    order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
+
+    public function shipping()
+    {
+        return $this->hasOne(Shipping::class, 'order_id');
+    }
 }
+
