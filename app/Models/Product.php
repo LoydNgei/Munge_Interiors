5befcb01faa2f2b487b0bf9e2b567d1fabe5468d
LoydNgei <loydngei@gmail.com>
2024-08-09 23:14:32 +0300

    product model well created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $primaryKey = 'product_id';
+
+    protected $fillable = [
+        'product_image',
+        'product_name',
+        'product_price',
+        'product_description',
+        'product_quantity',
+        'product_size',
+        'product_colour',
+        'product_status',
+    ];
+
+
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id');
