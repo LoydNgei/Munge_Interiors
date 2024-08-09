6a3dad3dd71fdd76a6286de136d2a20cc94704c3
LoydNgei <loydngei@gmail.com>
2024-08-09 23:15:08 +0300

    Shipping model

--- app/Models/Shipping.php
+++ app/Models/Shipping.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Shipping extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'shipping_id';
+
+    protected $fillable = [
+        'order_id',
+        'user_id',
+        'shipping_address',
+        'shipping_status',
+        'tracking number',
+        'shipped_at',
+        'delivered_at'
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
