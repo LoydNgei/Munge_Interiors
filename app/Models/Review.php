677a7460e9e278856604cbeec662de503d44bd2c
LoydNgei <loydngei@gmail.com>
2024-08-09 23:14:44 +0300

    Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'review_id';
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'rating',
+        'comment'
+    ];
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
