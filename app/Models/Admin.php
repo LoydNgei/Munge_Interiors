14d59089a029a5e7fdd09e844e3ea9ef117863ab
LoydNgei <loydngei@gmail.com>
2024-08-09 23:12:56 +0300

    Admin model modified

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admins extends Model
+class Admin extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'admin_id'; // This specifies the primary key column
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function someRelationship() {
+        
+    }
 }
