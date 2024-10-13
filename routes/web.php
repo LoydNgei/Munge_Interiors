380559831184bf90f8360b28b8a153f7f061485d
Loyd Ngei <loydngei@gmail.com>
2024-10-13 22:16:24 +0300

    Routes made better by grouping them

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\BillingController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,27 +19,43 @@ use App\Http\Controllers\PageController;
 */
 
 
+// Home
 Route::get('/', [HomeController::class, 'home']);
 
-Route::get('/shop', [PageController::class, 'shop']);
+// Display pages - PageController
+Route::controller(PageController::class)->group(function () {
+    Route::get('/shop', 'shop')->name('page.shop');
+    Route::get('/account', 'account')->name('page.account');
+    Route::get('/wishlist', 'wishlist')->name('page.wishlist');
+    Route::get('/checkout', 'checkout')->name('page.checkout');
+});
 
-Route::get('/account', [PageController::class, 'account']);
+// Product Controller
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/product', 'product');
+});
 
-Route::get('/wishlist', [PageController::class, 'wishlist']);
+// Billing Controller - Manage billing information
+Route::controller(BillingController::class)->group(function () {
+    Route::get('/billing', 'show')->name('billing.show');
+    Route::post('/billing', 'store')->name('billing.store');
+    Route::get('/billing/create', 'BillingController@create')->name('billing.create');
+    Route::get('/billing/{billing}/edit', 'BillingController@edit')->name('billing.edit');
+    Route::put('/billing/{billing}', 'update')->name('billing.update');
+    Route::delete('/billing/{billing}', 'destroy')->name('billing.destroy');
+});
 
-// Revisit
-Route::get('/product', [ProductController::class, 'product']);
+// Shipping Controller - Manage Shipping Information
+Route::controller(ShippingController::class)->group(function () {
 
-Route::get('/checkout', [PageController::class, 'checkout']);
-
-
+});
 
 
-
-
-// Authentication Routes
-
-Route::get('/login', [UserController::class, 'login']);
-
-Route::get('/register', [UserController::class, 'register']);
+// Authentication Routes - UserController
+Route::controller(UserController::class)->group(function () {
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'postlogin')->name('login.post');
+    Route::get('/register', 'register')->name('register');
+    Route::post('/register', 'postregister')->name('register.post');
+});
 
