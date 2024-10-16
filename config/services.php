4e4c6dee359eff13cc339fe93d5da5cbb1e0e3e1
Loyd Ngei <loydngei@gmail.com>
2024-10-16 23:16:59 +0300

    changed the path to the URL

--- config/services.php
+++ config/services.php
@@ -32,15 +32,14 @@ return [
     ],
 
     'google' => [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
-    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => '',
+        'redirect' => env('FACEBOOK_REDIRECT_URL'),
     ],
-
 ];
