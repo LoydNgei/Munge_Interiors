b685cb99f9ed8f5c33eadc72b40321d1963120db
Loyd Ngei <loydngei@gmail.com>
2024-11-02 22:47:13 +0300

    Register blade for users

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -50,16 +50,13 @@
                 <div class="text-gray-600 uppercase px-3 bg-white z-10 relative">Or signup with</div>
                 <div class="absolute left-0 top-3 w-full border-b-2 border-gray-200"></div>
             </div>
-            <div class="mt-4 flex gap-4">
-                {{-- <a href="#"
-                    class="w-1/2 py-2 text-center text-white bg-blue-800 rounded uppercase font-roboto font-medium text-sm hover:bg-blue-700">facebook</a> --}}
-                <a href="{{ route('auth.google') }}"
-                    class="py-2 text-center text-white bg-red-600 rounded uppercase font-roboto font-medium text-sm hover:bg-red-500">google</a>
+            <div class="mt-4">
+                <a href="{{ route('auth.google') }}" class="block w-full py-2 text-center text-white bg-red-600 rounded uppercase font-roboto font-medium text-sm hover:bg-red-500">google</a>
             </div>
             <!-- ./login with -->
-
-            <p class="mt-4 text-center text-gray-600">Already have account? <a href="/login"
-                    class="text-primary">Login now</a></p>
+            <p class="mt-4 text-center text-gray-600">Already have account?
+                <a href="/login" class="text-primary">Login now</a>
+            </p>
         </div>
     </div>
     <!-- ./login -->
