238d9a07aa4ea16a450aab0873c4fa6757be47a1
Loyd Ngei <loydngei@gmail.com>
2024-11-02 22:44:33 +0300

    Auth login middleware

--- app/Http/Middleware/EnsureLoggedIn.php
+++ app/Http/Middleware/EnsureLoggedIn.php
@@ -5,7 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
+
+
 class EnsureLoggedIn
 {
     /**
@@ -15,7 +17,7 @@ class EnsureLoggedIn
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->is('auth/google*') || $request->is('auth/facebook*'))
+        if ($request->is('auth/google*'))
         {
             return $next($request);
         }
