a6980d24f0e655fb578b8d944e0392e3cafe2f77
Loyd Ngei <loydngei@gmail.com>
2024-11-02 22:44:04 +0300

    Admin auth logic

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,25 +1,70 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
 
 class AdminController extends Controller
 {
-
-    public function display() {
-        return view('Admin.admin');
+    // Show register form if no admin exists
+    public function register() {
+        if (Admin::count() > 0) {
+            return redirect()->route('admin.login')->withErrors('Admin already exists. Please log in!');
+        }
+        return view('Admin.register');
     }
 
-    public function postproducts() {
+    // Register only if no admin exists
+    public function postRegister(Request $request) {
+        if (Admin::count() > 0) {
+            return redirect()->route('admin.login')->withErrors('Admin already exists. Please log in!');
+        }
+        // $request->validate([
+        //     'username' => 'required|unique:admins',
+        //     'password' => 'required|min:8|confirmed',
+        // ]);
 
+        Admin::create([
+            'username' => $request->username,
+            'password' => Hash::make($request->password,)
+        ]);
+        return redirect()->route('admin.login')->with('success', 'Admin registered successfully. Please log in!');
     }
-    
-    public function updateproducts() {
 
+    // Show login form
+    public function showLoginForm() {
+        return view('Admin.login');
     }
 
-    public function deleteproducts() {
+    // Handle login logic
+    public function login(Request $request) {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
 
+        $admin = Admin::where('username', $request->username)->first();
+
+        if (!$admin) {
+            return back()->withErrors('Username does not exist');
+        }
+
+        if (!Hash::check($request->password, $admin->password)) {
+            return back()->withErrors('Incorrect password');
+        }
+
+        session()->put('admin_logged_in', true);
+        return redirect()->route('admin.dashboard')->with('success', 'Logged in successfully');
+    }
+
+    // Show admin Dashboard
+    public function dashboard() {
+        return view('Admin.dashboard');
+    }
+
+    public function logout() {
+        session()->forget('admin_logged_in');
+        return redirect()->route('admin.login')->with('success', 'Logged out successfully!');
     }
 }
