10b772116b0159514fb2a624dfeb7866c68325fd
Loyd Ngei <loydngei@gmail.com>
2024-11-02 22:44:52 +0300

    Changed the Admin Models

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,15 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Admin extends Model
 {
     use HasFactory;
 
+    protected $table = 'admins';
     protected $primaryKey = 'admin_id'; // This specifies the primary key column
 
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
