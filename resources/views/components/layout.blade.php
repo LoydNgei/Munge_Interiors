f98cb9dded733a32510831f3d499b18b475beed1
Loyd Ngei <loydngei@gmail.com>
2024-11-05 23:47:48 +0300

    Layout blade added the links to alert.css and alert.js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,6 +13,9 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="{{ asset('css/alert.css') }}">
+        <script src="{{ asset('js/alert.js') }}" defer></script>
+
 
         @vite('resources/css/app.css')
         
